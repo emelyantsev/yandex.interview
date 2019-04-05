841a7c14a32da878a34109d262da55336839b71c
Aleksey Emelyantsev <35565870+EmelyantsevAA@users.noreply.github.com>
2019-04-05 14:21:33 +0300

    Accepted solution

---
+++ task_B/taskB.cpp
@@ -0,0 +1,52 @@
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    ifstream ifs;
+
+    ifs.open ("input.txt", std::ifstream::in);
+
+    size_t n;
+    ifs >> n;
+
+    vector<int> v;
+
+    for (size_t i = 0; i < n; ++i) {
+
+        int temp;
+        ifs >> temp;
+        v.push_back(temp);
+    }
+
+
+    int max_result = 0;
+    int current_result = 0;
+
+    for (auto it = v.begin(); it != v.end(); ++it ) {
+
+        int current = *it;
+
+        if (current == 1 ) {
+            ++current_result;
+        }
+        else {
+            current_result = 0;
+        } 
+
+        if (current_result > max_result) {
+            max_result = current_result;
+        }
+    }
+
+    ofstream ofs;
+
+    ofs.open ("output.txt", std::ofstream::out);
+
+    ofs << max_result << '\n';
+
+
+    return EXIT_SUCCESS;
+}
